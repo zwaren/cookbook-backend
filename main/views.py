1678791840a567408165e50f3e10a141de800e17
zwaren <zwaren@yandex.ru>
2019-11-14 20:47:55 +0400

    Add ws realtime update

--- main/views.py
+++ main/views.py
@@ -1,17 +1,34 @@
+import json
+
 from django.contrib.postgres.search import SearchVector
 from rest_framework import status
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from websocket import create_connection
 
 from .models import File, Recipe, RecipeStep
 from .serializers import FileSerializer, RecipeSerializer, RecipeStepSerializer
 
 
+def ws(data):
+    ws = create_connection("wss://cookback-ws.herokuapp.com/")
+    ws.send(data)
+    ws.close()
+
+
 class RecipeList(ListCreateAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        ws(json.dumps({
+            "type": "recipe",
+            "data": response.data
+        }))
+        return response
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -19,8 +36,8 @@ class RecipeList(ListCreateAPIView):
 
         if q is not None:
             return qs.select_related('author') \
-            .annotate(search=SearchVector('name', 'description', 'ingredients')) \
-            .filter(search=q)
+                .annotate(search=SearchVector('name', 'description', 'ingredients')) \
+                .filter(search=q)
 
         return Recipe.objects.all()
 
@@ -35,10 +52,10 @@ class FileUploadView(APIView):
 
     def post(self, request, *args, **kwargs):
 
-      file_serializer = FileSerializer(data=request.data)
+        file_serializer = FileSerializer(data=request.data)
 
-      if file_serializer.is_valid():
-          file_serializer.save()
-          return Response(file_serializer.data, status=status.HTTP_201_CREATED)
-      else:
-          return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if file_serializer.is_valid():
+            file_serializer.save()
+            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
