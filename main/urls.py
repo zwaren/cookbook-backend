b222dda405266a2794bdb8d6cdf5e1fee55545c3
zwaren <zwaren@yandex.ru>
2019-11-15 09:00:44 +0400

    fix

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('recipes', views.RecipeList.as_view()),
     path('recipe_steps', views.RecipeStepList.as_view()),
     path('upload', views.FileUploadView.as_view()),
+    path('vk', views.VkHook.as_view()),
 ]
